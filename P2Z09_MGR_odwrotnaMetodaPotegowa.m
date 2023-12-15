34ddfb3fd9f59636e8a5bc430b082ce63edd48f8
gawxgd <128865159+gawxgd@users.noreply.github.com>
2023-12-15 15:09:24 +0100

    poprawa specki

--- P2Z09_MGR_odwrotnaMetodaPotegowa.m
+++ P2Z09_MGR_odwrotnaMetodaPotegowa.m
@@ -1,23 +1,28 @@
-function [eigenValue] = P2Z09_MGR_odwrotnaMetodaPotegowa(A,epsilon,maxIT)
+function [eigenValue,error,itCount] = P2Z09_MGR_odwrotnaMetodaPotegowa(A,epsilon,maxIT)
     % Projekt 2, zadanie 09
     % Marcin Gronicki, 327351
     %
     % Próba znalezienia najmniejszej (co do wartości bezwględnej) wartości
     % własnej odwracalnej kwadratowej macierzy rzeczywistej lub zespolonej,
     % w przypadku podania na wejście macierzy o zerowym wyznaczniku 
-    % program zwraca []. Metoda przy rozwiąywaniu układów równań w każdej
-    % iteracji korzysta z obliczonego wcześniej rozkładu QR otrzymanego
-    % przy użyciu odbić Householdera
+    % program zwraca w każdym argumencie wyjściowym []. Metoda przy 
+    % rozwiązywaniu układów równań w każdej iteracji korzysta z obliczonego
+    % wcześniej rozkładu QR otrzymanego przy użyciu odbić Householdera
     % Wejście:
     %   - A - kwadratowa odwracalna macierz rzeczywista lub zespolona 
     %   - epsilon - tolerancja błędu
     %   - maxIT - maksymalna ilość iteracji
     % Wyjście:
     %   - eigenValue - szukana najmniejsza wartość własna co do modułu
+    %   - error - błąd między wartościami własnymi wyznaczonymi w dwóch
+    %   ostatnich iteracjach
+    %   - itCount - liczba wykonanych iteracji
     
     % Sprawdzenie czy macierz ma niezerowy wyznacznik
     if(det(A) == 0)
         eigenValue = [];
+        itCount = [];
+        error = [];
         return;
     end
     % Obliczenie rozkładu QR
@@ -47,4 +52,6 @@ function [eigenValue] = P2Z09_MGR_odwrotnaMetodaPotegowa(A,epsilon,maxIT)
     end
     % Odwrócenie znalezionej wartości własnej
     eigenValue = 1 / eigenValue;
+    itCount = it;
+    error = stopCondErr;
     
